ab63038dcd516537617656a2fca532f13b2054d6
Rinat <87980348+Rinat21072021@users.noreply.github.com>
2023-11-13 16:30:52 +0300

    Time: 119 ms (23.66%), Space: 47.1 MB (58.59%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,25 +3,24 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-  const legend = {
-    I:1,
-    V:5,
-    X:10,
-    L:50,
-    C:100,
-    D:500,
-    M:1000
-  };
-  let total = 0;
 
-  for (let i = 0; i < s.length; i++) {
-    if (legend[s[i]] < legend[s[i+1]]) {
-      total += legend[s[i+1]] - legend[s[i]];
-      i++;
+    let result=0;
+    let obj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+ for (let i = 0; i < s.length; i++) {
+    if (obj[s[i + 1]] > obj[s[i]]) {
+      result += obj[s[i + 1]] - obj[s[i]];
+        i++
     } else {
-      total += legend[s[i]];
+      result += obj[s[i]];
     }
   }
-
-  return total;
+  return result;
 };
